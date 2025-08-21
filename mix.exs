e3457186cead196a65a43bc35b9065001befd455
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-08-20 20:32:30 -0400

    update gh action and mix elixir and otp versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OpenDevCoach.MixProject do
     [
       app: :open_dev_coach,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
