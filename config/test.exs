da03b5125f5e86efb94c066c827ebc09fb111408
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-08-23 23:16:04 -0400

    remove comments

--- config/test.exs
+++ config/test.exs
@@ -1,11 +1,9 @@
 import Config
 
-# Configure your database
 config :open_dev_coach, OpenDevCoach.Repo,
   database: Path.expand("../open_dev_coach_test.db", Path.dirname(__ENV__.file)),
   pool: Ecto.Adapters.SQL.Sandbox
 
-# Configure Ecto for tests
 config :open_dev_coach,
   ecto_repos: [OpenDevCoach.Repo]
 
