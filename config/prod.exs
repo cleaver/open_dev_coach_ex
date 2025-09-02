52ccf3999d5910cd07852bda80f48fe5f6871e48
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-09-02 09:17:36 -0400

    fix prod config: - move env variable to runtime.exs

--- config/prod.exs
+++ config/prod.exs
@@ -5,9 +5,5 @@ config :open_dev_coach, OpenDevCoach.Repo,
   database: Path.expand("../open_dev_coach.db", Path.dirname(__ENV__.file)),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "5")
 
-# Configure timezone for production
-config :open_dev_coach,
-  timezone: System.get_env("TIMEZONE") || "America/New_York"
-
 # Do not print debug messages in production
 config :logger, level: :info
--- config/runtime.exs
+++ config/runtime.exs
@@ -1,13 +1,18 @@
 import Config
 
-log_level_env_var =
-  System.get_env("LOG_LEVEL", nil)
+if config_env() == :prod do
+  config :open_dev_coach,
+    timezone: System.get_env("TIMEZONE") || "America/New_York"
+else
+  log_level_env_var =
+    System.get_env("LOG_LEVEL", nil)
 
-if log_level_env_var do
-  log_level =
-    log_level_env_var
-    |> String.downcase()
-    |> String.to_existing_atom()
+  if log_level_env_var do
+    log_level =
+      log_level_env_var
+      |> String.downcase()
+      |> String.to_existing_atom()
 
-  config :logger, level: log_level
+    config :logger, level: log_level
+  end
 end
