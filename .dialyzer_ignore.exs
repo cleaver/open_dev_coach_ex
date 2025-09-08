a21bd3f2d4ac1de6fdb7474bddd3357ac55e20e3
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-09-08 14:09:18 -0400

    disable tio_comodo on test and iex

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,4 @@
+[
+  {"lib/open_dev_coach/application.ex", "Function IEx.started?/0 does not exist."},
+  {"lib/open_dev_coach/application.ex", "Function Mix.env/0 does not exist."}
+]
--- lib/open_dev_coach/application.ex
+++ lib/open_dev_coach/application.ex
@@ -15,16 +15,31 @@ defmodule OpenDevCoach.Application do
         end
       end)
 
-    children = [
-      OpenDevCoach.Repo,
-      OpenDevCoach.Session,
-      OpenDevCoach.Scheduler,
-      {TioComodo.Repl.Server, prompt: "opendevcoach> ", name: OpenDevCoach.Repl, parent: parent}
-    ]
+    # Build children list conditionally
+    children =
+      [
+        OpenDevCoach.Repo,
+        OpenDevCoach.Session,
+        OpenDevCoach.Scheduler
+      ] ++
+        maybe_start_repl(parent)
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: OpenDevCoach.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp maybe_start_repl(parent) do
+    iex_running? = Code.ensure_loaded?(IEx) and IEx.started?()
+    mix_env_test? = Code.ensure_loaded?(Mix) and Mix.env() == :test
+
+    if mix_env_test? or iex_running? do
+      []
+    else
+      [
+        {TioComodo.Repl.Server, prompt: "opendevcoach> ", name: OpenDevCoach.Repl, parent: parent}
+      ]
+    end
   end
 end
